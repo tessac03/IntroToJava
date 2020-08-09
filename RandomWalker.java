85de4db6c7ed1ca40d6224aa49a39218c53300c4
tessac03 <tcotron@gmail.com>
2020-08-09 16:07:43 -0400

    Update RandomWalker.java

--- RandomWalker.java
+++ RandomWalker.java
@@ -6,13 +6,12 @@
 
 public class RandomWalker {
     public static void main(String[] args){
-        //Creates variable n from the command line argument input to be used as the
+        //Create variable n from the command line argument input to be used as the
         //number of steps to take, and variables x,y to chart the random walk
         int n = Integer.parseInt(args[0]);
         int x=0, y=0;
         
-        //while loop to generate the n many random steps and to print out each point
-        //in the step
+        //while loop to generate n many random steps and print out each point
         while(n>0){
             System.out.println("("+x+", "+y+")");
             double r=Math.random();
@@ -22,7 +21,7 @@ public class RandomWalker {
             else y++;
             n--;
         }
-        //print out the final point in the random walk and compute and print out 
+        //print the final point in the random walk and compute and print out 
         //the squared distance
         System.out.println("("+x+", "+y+")");
         System.out.println("squared distance = "+((x*x)+(y*y)));
