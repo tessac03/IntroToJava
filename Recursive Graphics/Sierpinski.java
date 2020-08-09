92a1ce96dd1f75c7ac5c92c900a37584d4b93148
tessac03 <tcotron@gmail.com>
2020-08-09 18:06:47 -0400

    Add files via upload

---
+++ Recursive Graphics/Sierpinski.java
@@ -0,0 +1,42 @@
+public class Sierpinski {
+    //  Height of an equilateral triangle whose sides are of the specified length.
+    public static double height(double length){
+    	return (length*Math.sqrt(3))/2;
+    }
+
+    //  Draws a filled equilateral triangle whose bottom vertex is (x, y)
+    //  of the specified side length.
+    public static void filledTriangle(double x, double y, double length){
+    	double[]xcoord={x, x-(length/2), x+(length/2)};
+    	double[]ycoord={y,(y+height(length)),(y+height(length))};
+    	StdDraw.filledPolygon(xcoord, ycoord);
+    }
+
+    //  Draws a Sierpinski triangle of order n, such that the largest filled
+    //  triangle has bottom vertex (x, y) and sides of the specified length.
+    public static void sierpinski(int n, double x, double y, double length){
+    	//base case when level=1
+    	if(n==1){
+    		filledTriangle(x,y,length);
+    	}else{
+    		filledTriangle(x,y,length);
+    		sierpinski(n-1, x, y+height(length), length/2);
+    		sierpinski(n-1, x-length/2, y, length/2 );
+    		sierpinski(n-1, x+length/2, y, length/2);
+    	}
+    }
+
+    //  Takes an integer command-line argument n;
+    //  draws the outline of an equilateral triangle (pointed upwards) of length 1;
+    //  whose bottom-left vertex is (0, 0) and bottom-right vertex is (1, 0); and
+    //  draws a Sierpinski triangle of order n that fits snugly inside the outline.
+    public static void main(String[] args){
+    	int n=Integer.parseInt(args[0]);
+    	StdDraw.setXscale(-0.1,1.1);
+    	StdDraw.setYscale(-0.1, 1);
+    	double[]x={0,1,0.5};
+    	double[]y={0,0,Math.sqrt(3)/2};
+    	StdDraw.polygon(x, y);
+    	sierpinski(n,0.5,0,0.5);
+    }
+}
---
+++ Recursive Graphics/Transform2D.java
@@ -0,0 +1,40 @@
+public class Transform2D {
+    // Returns a new array object that is an exact copy of the given array.
+    // The given array is not mutated.
+    public static double[] copy(double[] array){
+    	double[]copy=array;
+    	return copy;
+    }
+
+    // Scales the polygon by the factor alpha.
+    public static void scale(double[] x, double[] y, double alpha){
+    	int i=0;
+    	while(i<x.length){
+    		x[i]=x[i]*alpha;
+    		y[i]=y[i]*alpha;
+    		i++;
+    	}   	
+    }
+
+    // Translates the polygon by (dx, dy).
+    public static void translate(double[] x, double[] y, double dx, double dy){
+    	int i=0;
+    	while(i<x.length){
+    		x[i]+=dx;
+    		y[i]+=dy;
+    		i++;
+    	}
+    }
+
+    // Rotates the polygon theta degrees counterclockwise, about the origin.
+    public static void rotate(double[] x, double[] y, double theta){
+    	int i=0;
+    	while(i<x.length){
+    		double xi=x[i];
+    		double yi=y[i];
+    		x[i]=xi*Math.cos(Math.toRadians(theta))-yi*Math.sin(Math.toRadians(theta));
+    		y[i]=yi*Math.cos(Math.toRadians(theta))+xi*Math.sin(Math.toRadians(theta));
+    		i++;
+    	}
+    }
+}
