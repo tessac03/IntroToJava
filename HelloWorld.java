06f0009b50fe2f0e54e28adbc4b32a88029c8794
tessac03 <tcotron@gmail.com>
2020-08-09 16:49:26 -0400

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-/* *************************************************************************************
- * Description: Prints out "Hello World" message.
- ************************************************************************************ */
-public class HelloWorld {
-	public static void main(String[] args){
-		System.out.println("Hello, World");
-	}
-}
